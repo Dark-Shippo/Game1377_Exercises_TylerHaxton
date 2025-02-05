0363a56b9acaaa4247412de986797b4efb5383ef
Dark-Shippo <tylerhaxton96@gmail.com>
2025-02-04 21:11:20 -0600

    Porperly implemented Console.ReadLine() into the excercise.

--- Fundamentals1/Program.cs
+++ Fundamentals1/Program.cs
@@ -23,27 +23,31 @@
 
         private static void ArithmeticAndArithmeticMethods()
         {
-            Console.WriteLine("Enter the first number");
-            int intInput1 = Console.Read();
+            Console.WriteLine("Enter a number");
+            string? stringInput1 = Console.ReadLine();
+            int intInput1 = Convert.ToInt32(stringInput1);
 
-            Console.WriteLine("Enter the second number");
-            int intInput2; 
-            intInput2 = Console.Read();
+            Console.WriteLine("Enter a second number");
+            string? stringInput2 = Console.ReadLine();
+            int intInput2 = Convert.ToInt32(stringInput2);
+
 
             Console.WriteLine("Addition = " + (intInput1 + intInput2));
             Console.WriteLine("Subtraction = " + (intInput1 - intInput2));
-            Console.WriteLine("Multiplication = " + intInput1 * intInput2);
+            Console.WriteLine("Multiplication = " + (intInput1 * intInput2));
             Console.WriteLine("Division = " + (Convert.ToSingle(intInput1) / intInput2));
-            Console.WriteLine("Modulo = " + intInput1 % intInput2);
+            Console.WriteLine("Modulo = " + (intInput1 % intInput2));
         }
 
         private static void FloatMethods()
         {
-            Console.WriteLine("Enter the first float");
-            float floatInput1 = Console.Read();
-
-            Console.WriteLine("Enter the second float");
-            float floatInput2 = Console.Read();
+            Console.WriteLine("Enter a number with decimals");
+            string? stringInput1 = Console.ReadLine();
+            float floatInput1 = Convert.ToSingle(stringInput1);
+               
+            Console.WriteLine("Enter a second number with decimals");
+            string? stringInput2 = Console.ReadLine();
+            float floatInput2 = Convert.ToSingle(stringInput2);
 
             Console.WriteLine("floatInput2 = " + floatInput2);
             floatInput2 = floatInput2 + floatInput1;
@@ -58,11 +62,13 @@
 
         private static void ComparisonsAndConditionals()
         {
-            Console.WriteLine("Enter the first number for comparison");
-            int intInput1 = Console.Read();
+            Console.WriteLine("Enter the first number");
+            string? stringInput1 = Console.ReadLine();
+            int intInput1 = Convert.ToInt32(stringInput1);
 
-            Console.WriteLine("Enter the second number for comparison");
-            int intInput2 = Console.Read();
+            Console.WriteLine("Enter the second number");
+            string? stringInput2 = Console.ReadLine();
+            int intInput2 = Convert.ToInt32(stringInput2);
 
             if (intInput1 == intInput2)
             {
